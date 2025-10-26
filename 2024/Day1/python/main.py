15aa5803152332747de9878f36bd6f18c57811ea
JoeChala <jchalakkal16@gmail.com>
2025-10-26 23:05:05 +0530

    added day1 part2

--- 2024/Day1/python/main.py
+++ 2024/Day1/python/main.py
@@ -9,11 +9,20 @@ def main() -> None:
     leng = len(left)-1
     quicksort(left,0,leng)
     quicksort(right,0,leng)
-
+    
+    #part 1 find sum of difference b/w smallest left element and smallest right element
     sum = 0
     for i in range(0,leng+1):
         sum += abs(left[i] - right[i])
-    print(sum)
+    print("Part 1: ",sum)
+
+    #part 2 find similarity score element in left * freq in right
+    sim_score = 0
+    for i in range(0,leng+1):
+        count = right.count(left[i])
+        sim_score += left[i] * count
+    print("Part 2: ",sim_score)
+
 
 def swap(arr:list,a : int,b : int) -> None:
     arr[a],arr[b] = arr[b],arr[a]
@@ -35,6 +44,9 @@ def partition(arr : list,low : int,high : int) -> int:
             i += 1
     swap(arr,i,high)
     return i
+
+
+
 
 if __name__ == "__main__":
     main()
